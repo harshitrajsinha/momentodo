a55ff276ca3c4f9c4852de04b63d2b12e45deea8
Harshit <raj.harshitsinha08@gmail.com>
2025-02-15 13:33:43 +0530

    create todo component creation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).mount('#app')
+// Add icons to the library
+library.add(faPlus);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
